226251588e0b4872c4f35807d31d390f3452df16
chenjunhong <chenjunhong@techstar.com.cn>
2023-12-04 15:49:40 +0800

    缓存工具格式调整

--- JavaDemo/src/main/java/com/cache/TimeExpiredPoolCache.java
+++ JavaDemo/src/main/java/com/cache/TimeExpiredPoolCache.java
@@ -8,12 +8,12 @@ import java.util.TimerTask;
 import java.util.concurrent.ConcurrentHashMap;
 
 /**
-  * @author chenjunhong
-  * @description TODO 缓存工具类
-  * @date 2022/5/3
-  */
+ * @author chenjunhong
+ * @description TODO 缓存工具类
+ * @date 2022/5/3
+ */
 public class TimeExpiredPoolCache {
-    private static  long defaultCachedMillis = 30 * 1000L;//过期时间默认30秒
+    private static long defaultCachedMillis = 30 * 1000L;//过期时间默认30秒
     private static long timerMillis = 1 * 60 * 1000L;//定时清理默认1分钟
     /**
      * 对象池
@@ -23,14 +23,17 @@ public class TimeExpiredPoolCache {
      * 对象单例
      */
     private static TimeExpiredPoolCache instance = null;
+
     public TimeExpiredPoolCache() {
         dataPool = new ConcurrentHashMap<String, DataWrapper<?>>();
         initTimer();
     }
+
     /**
      * 定时器定时清理过期缓存
      */
     private static Timer timer = new Timer();
+
     private static void initTimer() {
         timer.scheduleAtFixedRate(new TimerTask() {
             @Override
@@ -46,20 +49,21 @@ public class TimeExpiredPoolCache {
 
     /**
      * 缓存数据
-     * @param key key值
-     * @param data 缓存数据
+     *
+     * @param key          key值
+     * @param data         缓存数据
      * @param cachedMillis 过期时间
-     * @param dataRenewer 刷新数据
+     * @param dataRenewer  刷新数据
      * @return
      */
     @SuppressWarnings("unchecked")
     public <T> T put(String key, T data, long cachedMillis, DataRenewer<T> dataRenewer) throws Exception {
-        DataWrapper<T> dataWrapper = (DataWrapper<T>)dataPool.get(key);
+        DataWrapper<T> dataWrapper = (DataWrapper<T>) dataPool.get(key);
         if (data == null && dataRenewer != null) {
             data = dataRenewer.renewData();
         }
         //当重新获取数据为空，直接返回不做put
-        if (data == null){
+        if (data == null) {
             return null;
         }
         if (dataWrapper != null) {
@@ -71,16 +75,18 @@ public class TimeExpiredPoolCache {
         }
         return data;
     }
+
     /**
      * 直接设置缓存值和时间
+     *
      * @param key
      * @param data
      * @param cachedMillis
      * @return
      */
     @SuppressWarnings("unchecked")
-    public <T> T put(String key, T data, long cachedMillis) throws Exception{
-        DataWrapper<T> dataWrapper = (DataWrapper<T>)dataPool.get(key);
+    public <T> T put(String key, T data, long cachedMillis) throws Exception {
+        DataWrapper<T> dataWrapper = (DataWrapper<T>) dataPool.get(key);
         if (dataWrapper != null) {
             //更新
             dataWrapper.update(data, cachedMillis);
@@ -90,8 +96,10 @@ public class TimeExpiredPoolCache {
         }
         return data;
     }
+
     /**
      * 获取缓存
+     *
      * @param key
      * @param cachedMillis
      * @param dataRenewer
@@ -99,20 +107,22 @@ public class TimeExpiredPoolCache {
      */
     @SuppressWarnings("unchecked")
     public <T> T get(String key, long cachedMillis, DataRenewer<T> dataRenewer) throws Exception {
-        DataWrapper<T> dataWrapper = (DataWrapper<T>)dataPool.get(key);
+        DataWrapper<T> dataWrapper = (DataWrapper<T>) dataPool.get(key);
         if (dataWrapper != null && !dataWrapper.isExpired()) {
             return dataWrapper.data;
         }
         return put(key, null, cachedMillis, dataRenewer);
     }
+
     @SuppressWarnings("unchecked")
-    public <T> T get(String key){
-        DataWrapper<T> dataWrapper = (DataWrapper<T>)dataPool.get(key);
+    public <T> T get(String key) {
+        DataWrapper<T> dataWrapper = (DataWrapper<T>) dataPool.get(key);
         if (dataWrapper != null && !dataWrapper.isExpired()) {
             return dataWrapper.data;
         }
         return null;
     }
+
     /**
      * 清除缓存
      */
@@ -122,8 +132,8 @@ public class TimeExpiredPoolCache {
 
     /**
      * 删除指定key的value
-     * */
-    public void remove(String key){
+     */
+    public void remove(String key) {
         dataPool.remove(key);
     }
 
@@ -143,19 +153,24 @@ public class TimeExpiredPoolCache {
          * 缓存时间
          */
         private long cachedMillis;
+
         private DataWrapper(T data, long cachedMillis) {
             this.update(data, cachedMillis);
         }
+
         public void update(T data, long cachedMillis) {
             this.data = data;
             this.cachedMillis = cachedMillis;
             this.updateExpiredTime();
         }
+
         public void updateExpiredTime() {
             this.expiredTime = System.currentTimeMillis() + cachedMillis;
         }
+
         /**
          * 数据是否过期
+         *
          * @return
          */
         public boolean isExpired() {
@@ -165,6 +180,7 @@ public class TimeExpiredPoolCache {
             return true;
         }
     }
+
     /**
      * 数据构造
      */
@@ -178,7 +194,7 @@ public class TimeExpiredPoolCache {
     private static void clearExpiredCaches() {
         List<String> expiredKeyList = new LinkedList<String>();
 
-        for(Entry<String, DataWrapper<?>> entry : dataPool.entrySet()){
+        for (Entry<String, DataWrapper<?>> entry : dataPool.entrySet()) {
             if (entry.getValue().isExpired()) {
                 expiredKeyList.add(entry.getKey());
             }
