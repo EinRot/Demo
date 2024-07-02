9cb60576714c75f6e2f7e99bdb26c2821ce9509a
chenjunhong <chenjunhong@techstar.com.cn>
2024-07-02 11:08:45 +0800

    build测试

---
+++ MavenBuild/JDKBuild/src/main/java/com/build/Application.java
@@ -0,0 +1,16 @@
+package com.build;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Description
+ * @Author EinIce
+ * @Date 2024/7/2 10:42
+ **/
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class);
+    }
+}
